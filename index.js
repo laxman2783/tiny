dd324a9f41432b7c039e2913978747c496e02ca2
Laxman Thota <laxmant.thota@gmail.com>
2019-11-19 13:49:43 +0530

    added  console.log in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function tiny(string) {
+  console.log ('in Tiny String function');
     if (typeof string !== "string") throw new TypeError("Tiny wants a string.....!");
     return string.replace(/\s/g, "");
   };
