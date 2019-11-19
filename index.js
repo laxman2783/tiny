3a976df50d8b9c4912d113116678bd2827328add
Laxman Thota <laxmant.thota@gmail.com>
2019-11-19 13:47:42 +0530

    updated  type error with comment Please enter a string

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string.....!");
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string.....!.Please enter a String");
     return string.replace(/\s/g, "");
   };
   
